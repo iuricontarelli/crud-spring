97df77d244a9e54165964f87a8c1fa088ae78aa8
Iuri Alves Contarelli <iuri.contarelli@wmtrading.com.br>
2024-05-16 10:31:38 -0300

    18: Componente de Formulário e Roteamento para criar cursos

--- src/main/java/com/iuri/controller/CourseController.java
+++ src/main/java/com/iuri/controller/CourseController.java
@@ -2,6 +2,7 @@ package com.iuri.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +20,7 @@ public class CourseController {
 
     private final CourseRepository courseRepository;
 
+    @CrossOrigin
     @GetMapping
     public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
