a67ee5e036bb3366e746dbfec0d3cdf6c97bee4c
Iuri Alves Contarelli <iuri.contarelli@wmtrading.com.br>
2024-07-12 08:51:06 -0300

    21: Formulário: API Spring: Criar Curso (HTTP POST)

--- src/main/java/com/iuri/controller/CourseController.java
+++ src/main/java/com/iuri/controller/CourseController.java
@@ -2,10 +2,15 @@ package com.iuri.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.iuri.model.Course;
@@ -16,6 +21,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping("/api/courses")
 @AllArgsConstructor
+@CrossOrigin
 public class CourseController {
 
     private final CourseRepository courseRepository;
@@ -24,6 +30,15 @@ public class CourseController {
     @GetMapping
     public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        // System.out.println(course.getName());
+        return courseRepository.save(course);
+        // return ResponseEntity.status(HttpStatus.CREATED)
+        //     .body(courseRepository.save(course));
     }
 
 }
